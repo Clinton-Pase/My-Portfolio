1718002d569f0c440c5ddfe65b17afb5dcb3ba12
Clinton-Pase <167394500+Clinton-Pase@users.noreply.github.com>
2026-04-06 20:38:23 +0100

    Add files via upload

---
+++ My-Portfolio/Images/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: "class", // Enable dark mode via class
+  theme: {
+    extend: {
+
+      // Add Google Fonts here
+      fontFamily: {
+        'raleway': ['Raleway', 'sans-serif'],
+      
+      },
+      
+    
+    },
+  },
+  plugins: [],
+};
